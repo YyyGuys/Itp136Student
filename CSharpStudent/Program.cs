6f38224ef7725b97b7cb6e8cb54015af446af3b4
Bob Dust <bdust@reynolds.edu>
2022-08-22 16:30:30 -0400

    Add project files.

---
+++ CSharpStudent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# ____"); //--< Replace the "____" with your name <<<
+            Console.ReadKey();
+        }
+    }
+}
