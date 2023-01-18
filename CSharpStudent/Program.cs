8ed3bc8754fb3c92b2d57f54c18aaa303db0cfcf
Bob Dust <bdust@reynolds.edu>
2023-01-18 12:35:15 -0500

    Initial ITP-136 Solution Spring 2023

--- CSharpStudent/Program.cs
+++ CSharpStudent/Program.cs
@@ -8,9 +8,25 @@ namespace CSharpStudent
 {
     internal class Program
     {
+        /*
+         * In the Solution Explorer (probably in the right panel)
+         *      R-Click the Solution Name (Itp136Student)
+         *          Choose "Rename"
+         *          Change "Student" to "YourName" (no space or special characters)
+         *              For example: "Itp136ChrisJones"
+         *      R-Click the Project name (CSharpStudentYourName)
+         *          Choose "Rename"
+         *          Change "YourName" to your first and last name (no spaces or special characters)
+         *              For example: "Itp136StudentChrisJones"
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C# ____"); //--< Replace the "____" with your name <<<
+            int year = 2023;
+            Console.WriteLine("Welcome to C#, ____"); //--< Replace the "____" with your name <<<
+            Console.WriteLine($"The year is {year}");
+
+            year++;         //--< The ++ increments the year variable <<<
+            Console.WriteLine($"Next year is {year}");
             Console.ReadKey();
         }
     }
